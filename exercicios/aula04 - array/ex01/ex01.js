f6e5e8f2b8abf1fb978fc6913cb63e0909b480f5
IguSantos <130194711+IguSantos019@users.noreply.github.com>
2026-03-28 20:54:31 -0300

    estudando arrays

---
+++ exercicios/aula04 - array/ex01/ex01.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 4]
+num[3] = 6
+num[5] = 2
+num.push(9)
+num.sort()
+console.log(`o valor é ${num} o array num tem atualmete ${num.length} índices`)
+
---
+++ exercicios/aula04 - array/ex02.js
@@ -0,0 +1,5 @@
+let valores = [1,2,3,4,5,6]
+console.log(valores)
+for (var c = 0; c < valores.length; c++) {
+    console.log(`a posição ${c} tem o valor ${valores[c]}`)
+}
