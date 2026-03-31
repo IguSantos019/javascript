ef935b914d49948f5ca3bb50f4544815261c6519
Igor Silva <130194711+IguSantos019@users.noreply.github.com>
2026-03-31 01:17:27 -0300

    inicio das interatividades dos botoes

--- desafios/meus-desafios/calculadora/script.js
+++ desafios/meus-desafios/calculadora/script.js
@@ -1,23 +1,11 @@
-let um = document.getElementById('um')
-um = 1
-let dois = document.getElementById('dois')
-dois = 2 
-let tres = document.getElementById('dois')
-tres = 3
-let dois = document.getElementById('dois')
-quatro = 4
-let cinco = 5
-let dois = document.getElementById('dois')
-let seis = 6
-let dois = document.getElementById('dois')
-let sete = 7
-let dois = document.getElementById('dois')
-let oito = 8
-let dois = document.getElementById('dois')
-let nove = 9
-let dois = document.getElementById('dois')
-let zero = 0
-let dois = document.getElementById('dois')
+let resul = document.getElementById('resul')
 
+function insert(num) {
+    var numeros = num
+    resul.innerHTML += numeros
+    num = Number(num)
+    console.log(typeof num)
+    console.log(``)
+}
 
 
