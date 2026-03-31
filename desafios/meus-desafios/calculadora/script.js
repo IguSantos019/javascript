93aaa57a051dd4eb438e216cae6d8e38fa5d4a9f
Igor Silva <130194711+IguSantos019@users.noreply.github.com>
2026-03-31 00:43:51 -0300

    alteração nos cliques dos botoes

---
+++ desafios/meus-desafios/calculadora/script.js
@@ -0,0 +1,23 @@
+let um = document.getElementById('um')
+um = 1
+let dois = document.getElementById('dois')
+dois = 2 
+let tres = document.getElementById('dois')
+tres = 3
+let dois = document.getElementById('dois')
+quatro = 4
+let cinco = 5
+let dois = document.getElementById('dois')
+let seis = 6
+let dois = document.getElementById('dois')
+let sete = 7
+let dois = document.getElementById('dois')
+let oito = 8
+let dois = document.getElementById('dois')
+let nove = 9
+let dois = document.getElementById('dois')
+let zero = 0
+let dois = document.getElementById('dois')
+
+
+
