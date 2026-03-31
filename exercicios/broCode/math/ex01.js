1cfd3e98eca84bdf6c3dad183b435737e0ca61eb
IguSantos <130194711+IguSantos019@users.noreply.github.com>
2026-03-31 16:30:58 -0300

    começando ex de gerador aleatório de números

---
+++ exercicios/broCode/math/ex01.js
@@ -0,0 +1,57 @@
+// math = Objeto embutido que fornece uma coleção de propriedades e métodos.
+
+// math = built-in object that provides a collections of properties and methods
+
+/*
+    Math.PI = mostra o valor exato de PI
+
+    Math.round() = arredonda o valor para o valor mais próximo, por exemplo 3.99 arredonda para 4 ou, 3.20 arredonda para 3
+
+    Math.floor() = arredonda o valor para o inteiro menor mais próximo
+
+    Math.ceil() = arredonda o valor para o inteiro maior mais próximo
+
+    Math.trunc() = Retorna a parte inteira de um número, removendo os dígitos fracionários , independentemente de o número ser positivo ou negativo. Simplesmente corta tudo o que vem depois da vírgula decimal, sem arredondamento
+
+    Math.abs() = Retorna o valor inteiro de um número, por exemplo
+    Math.abs('-1'); // 1 
+
+    Math.pow(x, y) = Retorna a base x elevada à potência y do expoente
+    
+    Math.sqrt(x) = Retorna a raiz quadrada positiva de um número
+    Math.log(x) = Retorna o logaritmo natural
+    Math.sin(x) = retorna o seno de um número
+    Math.cos(x) = retorna o coseno de um número
+    Math.tan(x) = retorna a tangente de um número
+    
+    Math.sign(x) = retorna 1 se o número for positivo e maior que zero, retorna -1 se o número for negativo e maior que zero, e se for zero retorna zero
+
+    Math.max(x, y, z) = retorna o maior valor entre os parâmeros recebidos
+    Math.min(x, y, z) = retorna o menor valor entre os parâmeros recebidos
+    
+    Math.random() = Retorna um número pseudo-aleatório entre 0 e 1
+*/
+let x = 3
+let y = 2;
+let z = 8
+// console.log(Math.PI)
+// z = Math.round(x)
+// z = Math.floor(x)
+// z = Math.ceil(x)
+// z = Math.trunc(x)
+// z = Math.pow(x, y)
+//z = Math.sqrt(x)
+//z = Math.log(x)
+//z = Math.sin(x)
+//z = Math.cos(x)
+//z = Math.tan(x)
+//z = Math.abs(x)
+//z = Math.sign(x)
+//z = Math.log(x)
+
+/* let max = Math.max(x, y, z)
+console.log(max) */
+
+/*let min = Math.min(x, y, z)
+console.log(min) */
+
---
+++ exercicios/broCode/math/ex02.js
@@ -0,0 +1,6 @@
+// Random Number Generator
+const min = 10
+const max = 100
+let randomNum = Math.round(Math.random() * (max - min)) + min
+
+console.log(randomNum)
